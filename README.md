ae46550dd0ddb822694bf6deed9ce20b45133b7d
WeLytics <info@welytics.ai>
2020-01-04 17:23:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Prometheus Node Exporter Installation Script
+Prometheus installation script for node_exporter (https://github.com/prometheus/node_exporter) 
+
+How to use it:
+```
+sudo ./install.sh
+```
+
+Node_exporter service will be installed as systemd service, therefore common commands are working:
+
+### Start node_exporter service
+`sudo systemctl start node_exporter.service`
+
+### Stop node_exporter service
+`sudo systemctl stop node_exporter.service`
+
+### Restart node_exporter service
+`sudo systemctl restart node_exporter.service`
+
+### Status of node_exporter service
+`sudo systemctl status node_exporter.service`
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -x
+
+## simple check for root permissions
+if [ "$EUID" -ne 0 ]
+  then echo -e "\e[41mRun script as root\e[0m"
+  exit 1;
+fi
+
+## configs
+version="0.18.1"
+arch="amd64"
+install_dir="${BIN_DIR:-/usr/local/bin}"
+download_dir=$(dirname $(realpath $0))
+
+## setting up temp downlad and installation directory /tmp/node_exporter
+echo -e "\e[5mDownloading and installing..."
+
+cd /tmp 
+mkdir -p /tmp/node_exporter
+
+wget https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.linux-$arch.tar.gz -O node_exporter.tar.gz 
+
+echo -e "\e[5mExtracting... \e[25m"
+tar xfz /tmp/node_exporter.tar.gz -C /tmp/node_exporter ||  {  echo -e "\e[41mExtracting node_exporter.tar.gz failed! Exiting.\e[0m"; exit 1; }
+
+cp "/tmp/node_exporter/node_exporter-$version.linux-$arch/node_exporter" "$install_dir"
+
+chown root:root "$install_dir/node_exporter"
+
+cp $download_dir/node_exporter.service > /etc/systemd/system/node_exporter.service
+
+
+systemctl enable node_exporter.service
+systemctl start node_exporter.service
+
+echo -e " \e[25mInstallation \e[32msuccessful\e[0m"
